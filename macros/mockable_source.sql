9d151d07782ffd73a900b7de95f7d57ab7c5e950
afragne@ippon.fr <afragne@ippon.fr>
2023-10-17 09:54:07 +0000

    finish macro

--- macros/mockable_source.sql
+++ macros/mockable_source.sql
@@ -1,8 +1,8 @@
 
 {% macro mockable_source(source_name,table_name,seed_name) %}
     {% if target.name.lower() == 'ci' %}
-    from {{ref( seed_name )}}
+     {{ref(seed_name)}}
     {% else %}
-    from {{source(source_name, table_name)}}
+     {{source(source_name, table_name)}}
     {% endif %}
 {% endmacro %}
